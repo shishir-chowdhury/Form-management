5877180d5b52dd9fce2a38e26ef41e9b1b9d5d51
Shishir <shiishir@gmail.com>
2021-08-17 13:00:03 +0600

    Reciving data by post method

--- index.php
+++ index.php
@@ -8,14 +8,19 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
+    <?php
+         $student_name = $_POST['sname'];
+         $student_roll = $_POST['sroll'];
+         $student_cell = $_POST['scell'];
+    ?>
     <div class="log">
         <h2>Add new student</h2>
         <hr>
-        <form action="">
-        <input type="text" placeholder="Student Name">
-        <input type="text" placeholder="Student Roll">
-        <input type="text" placeholder="Student Cell">
-        <input type="submit" value="Add Student">
+        <form action="" method="POST">
+        <input name="sname"type="text" placeholder="Student Name">
+        <input name="sroll"type="text" placeholder="Student Roll">
+        <input name="scell"type="text" placeholder="Student Cell">
+        <input name="student_entry"type="submit" value="Add Student">
         </form>
     </div>
     
