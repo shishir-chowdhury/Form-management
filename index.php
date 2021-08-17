d99b886d30734b364860979951f6c5b7e807c064
Shishir <shiishir@gmail.com>
2021-08-17 11:58:37 +0600

    Initialize the files and simple form design

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Entry</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="log">
+        <h2>Add new student</h2>
+        <hr>
+        <input type="text" placeholder="Student Name">
+        <input type="text" placeholder="Student Roll">
+        <input type="text" placeholder="Student Cell">
+        <input type="submit" value="Add Student">
+    </div>
+    
+</body>
+</html>
